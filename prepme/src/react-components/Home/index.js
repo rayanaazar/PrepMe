b372302f0f3b667d742cc3bea313b1fa41080adb
Shayanlion <Shayanlion25@gmail.com>
2020-02-21 18:08:45 -0500

    Merged with Homepage commit to implement basic routing.

--- prepme/src/react-components/Home/index.js
+++ prepme/src/react-components/Home/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import "./styles.css";
+import LoginBox from "../LoginBox/LoginBox";
 
 /* Component for the Home page */
 class Home extends React.Component {
   render() {
     return (
-      <div className="home">
-        Homepage
+      <div className="App">
+        <LoginBox />
       </div>
     );
   }
