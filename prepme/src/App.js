04f6df2c1db202e712a6c417525c39748e4e36b3
Manveer Basra <m13basra@gmail.com>
2020-02-21 18:29:51 -0500

    Renamed Home to LoginPage

--- prepme/src/App.js
+++ prepme/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
+import LoginPage from './react-components/LoginPage';
 import Home from './react-components/Home';
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <BrowserRouter>
           <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
             { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+            <Route exact path='/' render={() => 
+                            (<LoginPage />)}/>
             <Route exact path='/' render={() => 
                             (<Home />)}/>
           </Switch>
--- prepme/src/react-components/LoginPage/index.js
+++ prepme/src/react-components/LoginPage/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import "./styles.css";
-import EventCard from "../EventCard/index";
+import LoginBox from "../LoginBox/LoginBox";
 
 /* Component for the Login page */
 class LoginPage extends React.Component {
   render() {
     return (
       <div className="App">
-        <EventCard title="CSC309" />
+        <LoginBox />
       </div>
     );
   }
