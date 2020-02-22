800bd1c2085fa78771f72115cc024ee1347d73f6
Shayanlion <Shayanlion25@gmail.com>
2020-02-22 12:37:51 -0500

    Passing state to child components in App.js

--- prepme/src/App.js
+++ prepme/src/App.js
@@ -21,9 +21,9 @@ class App extends React.Component {
                     <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
                         { /* Each Route below shows a different component depending on the exact path in the URL  */ }
                         <Route exact path='/' render={() =>
-                            (<LoginPage isAdmin={this.state.isAdmin}/>)}/>
+                            (<LoginPage state={this.state}/>)}/>
                         <Route exact path='/home' render={() =>
-                            (<Home />)}/>
+                            (<Home state={this.state}/>)}/>
                     </Switch>
                 </BrowserRouter>
             </div>
