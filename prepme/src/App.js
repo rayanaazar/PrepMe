3bc7c135d80b803a97ce809608fbdc6039a51108
Shayanlion <Shayanlion25@gmail.com>
2020-02-22 12:15:43 -0500

    Added material-ui/core to package.json

--- prepme/src/App.js
+++ prepme/src/App.js
@@ -6,20 +6,22 @@ import './App.css';
 import LoginPage from './react-components/LoginPage';
 import Home from './react-components/Home';
 
-function App() {
-  return (
-    <div>
-        <BrowserRouter>
-          <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
-            { /* Each Route below shows a different component depending on the exact path in the URL  */ }
-            <Route exact path='/' render={() => 
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
+                        { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+                        <Route exact path='/' render={() =>
                             (<LoginPage />)}/>
-            <Route exact path='/home' render={() => 
+                        <Route exact path='/home' render={() =>
                             (<Home />)}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
-  );
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
