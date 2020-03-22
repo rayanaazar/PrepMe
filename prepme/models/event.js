a66d2cff90591bf9611285f7da95f6b07e878310
Shayanlion <Shayanlion25@gmail.com>
2020-03-21 22:18:05 -0400

    Created basic mongoose schema for an Event object that fits our current event objects in the frontend. This can later be expanded to add validators for certain things such as validating usernames, course code formats, etc. In addition, we can later add more fields to the schema such as an array of file paths, or lat/long for google maps integration.

---
+++ prepme/models/event.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+    course: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    subject: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    location: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    date: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    time: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    size: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    members: {
+        type: [String],
+        required: true,
+    }
+});
+
+const Event = mongoose.model('event', EventSchema);
+module.exports = { Event };
