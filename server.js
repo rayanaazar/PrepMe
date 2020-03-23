a118d7af1268606f98ebbe72994616b364c134c2
Shayanlion <Shayanlion25@gmail.com>
2020-03-23 15:57:16 -0400

    Added post handler for /events route in order to add events to the database.

--- server.js
+++ server.js
@@ -17,8 +17,24 @@ app.use(bodyParser.json());
 
 app.get('/events', (req, res) => {
     Event.find().then((events) => {
-        res.send({ events }) // can wrap in object if want to add more properties
+        res.send(events) // can wrap in object if want to add more properties
     }, (error) => {
         res.status(500).send(error) // server error
     })
+});
+
+app.post('/events', (req, res) => {
+    const event = new Event(req.body);
+    log(req.body);
+
+    event.save().then((result) => {
+        res.send(result)
+    }, (error) => {
+        res.status(400).send(error) // 400 for bad request
+    })
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    log(`Listening on port ${port}...`)
 });
