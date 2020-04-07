292a837ce9453d555a2b5f12f4f559666cbdd052
Manveer Basra <m13basra@gmail.com>
2020-04-07 13:50:21 -0400

    Undid changes

--- prepme/src/actions/users.js
+++ prepme/src/actions/users.js
@@ -13,10 +13,7 @@ export const readCookie = (app) => {
       })
       .then(json => {
           if (json && json.currentUser) {
-              app.setState({ 
-                currentUser: json.currentUser,
-                isAdmin: json.isAdmin
-             });
+              app.setState({ currentUser: json.currentUser });
           }
       })
       .catch(error => {
